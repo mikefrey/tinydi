9cde16467f581597d9c8d4abb64a99da79959a02
Mike Frey <mike.frey@sportngin.com>
2014-04-14 09:37:06 -0500

    complete, untested

--- tinydi.js
+++ tinydi.js
@@ -1,12 +1,11 @@
-
-var registry = {}
-
 var di = {
 
+  runners: [],
   registry: {},
 
   reg: function(name, fn) {
     var args = di._getArgs(fn)
+    if (Array.isArray(fn)) fn = fn.shift()
     di.registry[name] = {
       fn: fn,
       args: args,
@@ -16,16 +15,44 @@ var di = {
   },
 
   run: function(fn) {
-    if (typeof fn === 'string') {
-      return di.exec(fn)
-    }
+    var args = di._getArgs(fn)
+    if (Array.isArray(fn)) fn = fn.shift()
+    di.runners.push({ fn: fn, args: args })
+    return di
   },
 
-  exec: function(name) {
-    // get fn by name and exec, cacheing the result in the registry
+  start: function() {
+    di.runners.forEach(function(entry) {
+      di._exec(entry)
+    })
+  },
+
+  _exec: function(entry) {
+    var deps = di._resolve(entry.args)
+    var inst = entry.fn.apply(null, deps)
+    if (entry.name) entry.instance = inst
+    return inst
+  },
+
+  _resolve: function(args) {
+    var deps = args.map(function(arg) {
+      var entry = di.registry[arg]
+
+      if (entry && entry.instance)
+        return entry.instance
+
+      if (entry)
+        return di._exec(entry)
+
+      if (window[arg])
+        return window[arg]
+
+      throw new Error('Cannot resolve dependency "' + arg '"')
+    })
   },
 
   _getArgs: function(fn) {
+    if (Array.isArray(fn)) return fn.slice(0, fn.length-2)
     var args = []
     var rx = /^function[^\(]*\(([^\)]*)\)/i
     var argStr = rx.match(fn.toString())[1]
